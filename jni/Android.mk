acf0447e103c6fb7b502fac56f74639c5b9bd4e6
Uriel Avalos <uriavalos@yahoo.com>
2011-08-29 22:29:00 -0400

    Mon Aug 29 22:29:00 EDT 2011

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,4 +1,21 @@
-#include $(all-subdir-makefiles)
+# Copyright 2011 Uriel Avalos and Frank and Robot Productions
+
+# This software uses libraries from FFmpeg licensed under the LGLv2.1.
+
+# This file is part of FFvideo Live Wallpaper.
+
+# FFvideo Live Wallpaper is free software: you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# FFvideo Live Wallpaper is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with FFvideo Live Wallpaper.  If not, see <http://www.gnu.org/licenses/>.
 
 LOCAL_PATH := $(call my-dir)
 
@@ -18,11 +35,6 @@ LOCAL_MODULE := bambuser-libavcodec
 LOCAL_SRC_FILES := $(MY_LIB_PATH)/libavcodec.so
 include $(PREBUILT_SHARED_LIBRARY)
 
-# include $(CLEAR_VARS)
-# LOCAL_MODULE := bambuser-libavdevice
-# LOCAL_SRC_FILES := $(MY_LIB_PATH)/libavdevice.so
-# include $(PREBUILT_SHARED_LIBRARY)
-
 include $(CLEAR_VARS)
 LOCAL_MODULE := bambuser-libavfilter
 LOCAL_SRC_FILES := $(MY_LIB_PATH)/libavfilter.so
@@ -37,7 +49,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := bambuser-libswscale
 LOCAL_SRC_FILES := $(MY_LIB_PATH)/libswscale.so
 include $(PREBUILT_SHARED_LIBRARY)
-
 
 #local_PATH := $(call my-dir)
 
