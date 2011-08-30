098f758aee9ace31d0d5e3b775712ebddd00c67c
Uriel Avalos <uriavalos@yahoo.com>
2011-08-29 22:30:31 -0400

    Mon Aug 29 22:30:31 EDT 2011

--- src/ffvideolivewallpaper/frankandrobot/com/VideoWallpaperSettings.java
+++ src/ffvideolivewallpaper/frankandrobot/com/VideoWallpaperSettings.java
@@ -1,3 +1,22 @@
+// Copyright 2011 Uriel Avalos and Frank and Robot Productions
+
+// This software uses libraries from FFmpeg licensed under the LGLv2.1.
+
+// This file is part of FFvideo Live Wallpaper.
+
+// FFvideo Live Wallpaper is free software: you can redistribute it
+// and/or modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+
+// FFvideo Live Wallpaper is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with FFvideo Live Wallpaper.  If not, see <http://www.gnu.org/licenses/>.
+
 package ffvideolivewallpaper.frankandrobot.com;
 
 import android.content.SharedPreferences;
